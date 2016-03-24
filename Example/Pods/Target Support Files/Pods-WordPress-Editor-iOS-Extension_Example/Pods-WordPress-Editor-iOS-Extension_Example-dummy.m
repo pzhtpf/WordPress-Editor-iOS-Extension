e6fba0edf532f201765f43a6a99eac9d220f170a
pzhtpf <389744841@qq.com>
2016-03-24 11:12:23 +0800

    Initial Commit of Library
    Signed-off-by: pzhtpf <389744841@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-WordPress-Editor-iOS-Extension_Example/Pods-WordPress-Editor-iOS-Extension_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WordPress_Editor_iOS_Extension_Example : NSObject
+@end
+@implementation PodsDummy_Pods_WordPress_Editor_iOS_Extension_Example
+@end
---
+++ Example/Pods/Target Support Files/WordPress-Editor-iOS-Extension/WordPress-Editor-iOS-Extension-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double WordPress_Editor_iOS_ExtensionVersionNumber;
+FOUNDATION_EXPORT const unsigned char WordPress_Editor_iOS_ExtensionVersionString[];
+
